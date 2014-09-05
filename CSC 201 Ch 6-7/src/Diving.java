9411d88f7606f18121abfc14e262127213d749c9
Destroierrr9 <Destroierrr9@Destroierrr9-HP.home>
2014-09-04 20:27:00 -0400

    Assignment 2

--- CSC 201 Ch 6-7/src/Diving.java
+++ CSC 201 Ch 6-7/src/Diving.java
@@ -2,61 +2,80 @@ import java.util.Scanner;
 public class Diving 
 {
 	private static double validScore, degreeOfDifficulty, totalScore, max=0, min=0;
-	private static int index, setValuesOnce=0;
-	private static double[] test=new double[7]; 
+	private static int index;
+	private static double[] divingScores=new double[7]; 
 	public static Scanner keyboard = new Scanner(System.in);
 	public static void main(String[]args)
 	{
 		inputAllScores();
 		calculateScore();
-		System.out.println(totalScore);
-		System.out.println(max);
-		System.out.println(min);
+		System.out.println("The final score is: "+totalScore);
 	}
 	private static double inputValidScore()
 	{
-		validScore = keyboard.nextDouble();
-		if (setValuesOnce==0)//sort array
+		do
 		{
-		max=validScore;
-		min=validScore;
-		setValuesOnce++;
-		}
-		if (validScore>max)
-		{
-			max=validScore;
-		}
-		else if (validScore<min)
-		{
-			min=validScore;
-		}
+			System.out.println("Input valid score between 0 and 10.");
+			validScore = keyboard.nextDouble();
+			if(validScore<0||validScore>10)
+				System.out.println("Invalid Input");
+		}while(validScore<0||validScore>10);//Checks for valid score
 		return validScore;
 	}
 	private static double[] inputAllScores()
 	{
-		for (index=0;index<7;index++)
+		inputValidDegreeOfDifficulty();
+		for (index=0;index<7;index++)//fills array with valid scores
 		{
 			inputValidScore();
-			test[index]=validScore;
+			divingScores[index]=validScore;
 		}
-		return test;
+		//sorts array from lowest to highest
+		boolean lowestToHighest; 
+		double temporaryNumber;
+		do
+		{
+			for(int i=0; i<=divingScores.length-2;i++)
+			{
+				
+				int nextNumber=i+1;
+				if(divingScores[i]>divingScores[nextNumber])
+				{
+					temporaryNumber=divingScores[nextNumber];
+					divingScores[nextNumber]=divingScores[i];
+					divingScores[i]=temporaryNumber;
+				}
+			}
+			lowestToHighest = divingScores[0]<divingScores[1]&&divingScores[1]<divingScores[2]&&divingScores[2]<divingScores[3]&&divingScores[3]<divingScores[4]&&divingScores[4]<divingScores[5]&&divingScores[5]<divingScores[6]&&divingScores[6]>divingScores[0];
+		}while(lowestToHighest==false);
+//		System.out.println(divingScores[0]);
+//		System.out.println(divingScores[1]);
+//		System.out.println(divingScores[2]);
+//		System.out.println(divingScores[3]);
+//		System.out.println(divingScores[4]);
+//		System.out.println(divingScores[5]);
+//		System.out.println(divingScores[6]);
+		return divingScores;
 	}
 	private static double inputValidDegreeOfDifficulty()
 	{
-		degreeOfDifficulty=keyboard.nextDouble();
+		do
+		{
+			System.out.println("Input valid degree of difficulty between 1.2 and 3.8");
+			degreeOfDifficulty=keyboard.nextDouble();
+			if(degreeOfDifficulty<1.2||degreeOfDifficulty>3.2)
+				System.out.println("Invalid Input");
+		}while(degreeOfDifficulty<1.2||degreeOfDifficulty>3.2);//Checks for valid degree of difficulty
 		return degreeOfDifficulty;
 	}
 	private static double calculateScore()
 	{
 		double sum=0;
-		inputValidDegreeOfDifficulty();
-		for(int i=0;i<test.length;i++)
+		for(int i=1;i<(divingScores.length-1);i++)
 		{
-			sum=sum+test[i];
+			sum=sum+divingScores[i];
 		}
-		totalScore=sum-(max+min);
-		totalScore=sum*degreeOfDifficulty;
-		totalScore*=0.6;
+		totalScore=(sum*degreeOfDifficulty)*0.6;
 		return totalScore;
 	}
 }
--- CSC 201 Ch 6-7/src/GirlScoutCookieSales.java
+++ CSC 201 Ch 6-7/src/GirlScoutCookieSales.java
@@ -1,115 +1,87 @@
+//Richard Cunningham
+//Create a main method that will solve the previous problem statement.
+//You may only use statements that are discussed in the book through Chapter 6.
+//You must not use any Array class or Array method included in the Java libraries to solve this problem.
+//Use the following input values for the final test of this program:
+//Total number of girls in the troop 8
+//Boxes of cookies for girl #1            43
+//Boxes of cookies for girl #2            18
+//Boxes of cookies for girl #3            23
+//Boxes of cookies for girl #4            20
+//Boxes of cookies for girl #5            35
+//Boxes of cookies for girl #6            17
+//Boxes of cookies for girl #7            11
+//Boxes of cookies for girl #8            25
+//Save your complete output to a text file to turn in with this assignment.
+//Upload your Java source code file and your output text file in Blackboard using the Assignment Upload area below.
+
+//Ask user for input on how many girls are in troop
+//make array length based on that number
+//ask user to input each amount of cookies sold for each girl
+//use another to count the ranges 
+//output results
 import java.util.Scanner;
 public class GirlScoutCookieSales {
 	public static Scanner keyboard = new Scanner(System.in);
-	private static int numberOfTroops;
-	private static int numberOfBoxes;
-	private static int numberOfCookiesSold;
-	private static int arrayLength;
-	private static int[] soldCookiesRanges = new int[0];
-	private static int[] girlScoutTroops = new int[0];
-	private static String output = " ";
-	/*public GirlScoutCookieSales (int numberOfTroops, int numberOfBoxes)
-	{
-		this.numberOfTroops=numberOfTroops;
-		this.numberOfBoxes=numberOfBoxes;
-	}*/
+	private static int numberOfTroops;//Population of troop
+	private static int[] soldCookiesRanges = new int[5];//counts the amount of cookies per range
+	private static int[] girlScoutTroops = new int[0];//troops amounts of cookies per girl
+
+
 	public static void main(String[]args)
 	{
 		System.out.println("Input number of girls in troops:");
-		numberOfTroops = keyboard.nextInt();
-		girlScoutTroops = new int[numberOfTroops];
+		do
+		{
+			numberOfTroops = keyboard.nextInt();//Determines length of array
+			if(numberOfTroops<=0)
+				System.out.println("Please input number greater than 0.");
+		}while(numberOfTroops<=0);//makes user input till > 0 troops can not be negative or zero
+		girlScoutTroops = new int[numberOfTroops];//sets array to length of troop size
 		///////////////////////////////////////////////
-		int max = 0;
-		for(int i=0;i!=girlScoutTroops.length;i++)
+
+		for(int i=0;i!=girlScoutTroops.length;i++)//go through array for input of cookies for each girl
 		{
 			System.out.println("Input boxes of cookies for girl # "+(i+1)+": ");
-			girlScoutTroops[i]=keyboard.nextInt();
-			if (girlScoutTroops[i]>max)
-				max = girlScoutTroops[i];
-		}
-		//////////////////////////////////////////////
-		int highestSold = max;
-		while (max>0)
-		{
-			if (max%10==0)
+			do
 			{
-				max-=10;
-				arrayLength++;
-			}
-			else if (max%10!=0)
-			{
-				int difference = max%10;
-				max -= difference;
-				arrayLength++;
-			}
-		}
-		soldCookiesRanges = new int[arrayLength];		
-		//////////////////////////////////////////////
-		int rangeNumberLow=0, rangeNumberHigh = 10;
-		for (int i=0;i!=soldCookiesRanges.length;i++)
-		{
-			if (girlScoutTroops[i]>rangeNumberLow&&girlScoutTroops[i]<=rangeNumberHigh)
-			{
-				soldCookiesRanges[i]++;
-			}
-					
+				girlScoutTroops[i]=keyboard.nextInt();//Amount of cookies per girl
+				if(girlScoutTroops[i]<0)
+					System.out.println("Please input postive numbers only");
+			}while(girlScoutTroops[i]<0);//makes user input till >=0 can not negative sold cookies
+
+			//determines which range of cookies sold and increments certain value of range array
+			if (girlScoutTroops[i]>0&&girlScoutTroops[i]<=10)
+				soldCookiesRanges[0]++;//increments 0 to 10
+			else if (girlScoutTroops[i]>=11&&girlScoutTroops[i]<=20)
+				soldCookiesRanges[1]++;//increments 11 to 20
+			else if (girlScoutTroops[i]>=21&&girlScoutTroops[i]<=30)
+				soldCookiesRanges[2]++;//increments 21 to 30
+			else if (girlScoutTroops[i]>=31&&girlScoutTroops[i]<=40)
+				soldCookiesRanges[3]++;//increments 31 to 40
+			else if (girlScoutTroops[i]>=41)
+				soldCookiesRanges[4]++;//increments 41 or more
 		}
 		/////////////////////////////////////////////
-		
-		//soldCookies = new int[arrayLength];
-		//System.out.println(soldCookies.length);
-		//InputSoldCookies();
-		System.out.println(output);
-		
+
+		output();
+
+
 	}
-	private static int DetermineArrayLength()
+	private static void output()//outputs results of Range array
 	{
-		int boxes = numberOfBoxes;
-		while (boxes>0)
-		{
-			if (boxes%10==0)
-			{
-				boxes-=10;
-				arrayLength++;
-			}
-			else if (boxes%10!=0)
-			{
-				int difference = boxes%10;
-				boxes -= difference;
-				arrayLength++;
-			}
-		}
-		return arrayLength;
+		System.out.println("0 to 10 cookies: "+soldCookiesRanges[0]);
+		System.out.println("11 to 20 cookies: "+soldCookiesRanges[1]);
+		System.out.println("21 to 30 cookies: "+soldCookiesRanges[2]);
+		System.out.println("31 to 40 cookies: "+soldCookiesRanges[3]);
+		System.out.println("41 or more cookies: "+soldCookiesRanges[4]);
 	}
-/*	private static int InputSoldCookies()
-	{
-		int index = 0, min = 0,max=numberOfBoxes;
-		
-		while (index!=soldCookies.length)
-		{
-			soldCookies[index]=keyboard.nextInt();
-			output = output+"\n"+soldCookies[index]+"\n";
-			index++;
-		}
-		return numberOfCookiesSold;
-	}*/
+
+	//getters and setters
 	public int getNumberOfTroops() {
 		return numberOfTroops;
 	}
 	public void setNumberOfTroops(int numberOfTroops) {
 		this.numberOfTroops = numberOfTroops;
 	}
-	public int getNumberOfBoxes() {
-		return numberOfBoxes;
-	}
-	public void setNumberOfBoxes(int numberOfBoxes) {
-		this.numberOfBoxes = numberOfBoxes;
-	}
-	public int getNumberOfCookiesSold() {
-		return numberOfCookiesSold;
-	}
-	public void setNumberOfCookiesSold(int numberOfCookiesSold) {
-		this.numberOfCookiesSold = numberOfCookiesSold;
-	}
-
 }
